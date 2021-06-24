d3ce022e5372a2d7edcd1ea8d788e48306a5f2ae
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-06-24 22:32:13 +0200

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+const darkbutton = document.getElementById("dark");
+const lightbutton = document.getElementById("light");
+const solarbutton = document.getElementById("solar");
+const body = document.body;
+
+darkbutton.onclick = () =>{
+    body.classList.replace("light","dark");
+}
+
+lightbutton.onclick = () =>{
+    body.classList.replace("dark","light");
+}
+
+solarbutton.onclick = ()=>{
+    if(body.classList.contains("solar")){
+        body.classList.remove("solar");
+        solarbutton.style.cssText = `
+            --bg-solar: var(--yellow);
+        `
+        solarbutton.innerText = "solarize";
+    }else{
+        solarbutton.style.cssText= `
+            --bg-solar:white;
+        `
+        body.classList.add("solar");
+        solarbutton.innerText = "solar";
+    }
+}
